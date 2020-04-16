4a79065d5fe8cb42ce887428de867b13973f2684
Parakhina Maria <mickeymouse0243@gmail.com>
2020-04-16 22:42:33 +0300

    lesson 16 / hello()

--- script/script.js
+++ script/script.js
@@ -1,30 +1,16 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function(){
-    const DOMElement = function(selector, height, width, bg, fontSize){
-        console.log(selector[0]);
-        let el;
-        if(selector[0] === '#'){
-            el = document.createElement('p');
-            el.id = selector.slice(1);
-        } else if(selector[0] === '.'){
-            el = document.createElement('div');
-            el.classList.add(selector.slice(1));
-            
-        }
-        height = height;
-        width = width;
-        bg = bg;
-        fontSize = fontSize;
-        el.style.cssText = `height:${height}; 
-                            width:${width};
-                            background-color:${bg};
-                            font-size:${fontSize};
-                            display:block;
-                            position:absolute;`;
-        document.body.prepend(el);
-        console.log(document.body);
-    };
-    const element = new DOMElement('.eldfgdf','100px','100px','red','2em');
-    console.log(element);
-
-})
+class First{
+    constructor() {
+      this.text = 'Привет я метод родителя!';
+    }
+    hello(){
+        console.log(this.text);
+    }
+}
+class Second extends First{
+     hello(){
+         console.log(this.text + ' А я наследуемый метод ');
+     }
+}
+let call =  new Second();
+call.hello();
